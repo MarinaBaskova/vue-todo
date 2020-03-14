e3d228c7ae7ce70bb2a1ca20846454c82c947fec
Marina Baskova <marinabaskova24@gmail.com>
2020-03-14 14:56:06 -0400

    setup todos component

---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+  </div>
+</template>
+
+<!--add component name to export -->
+<script>
+export default {
+  name: "Todos"
+};
+</script>
+
+<style scoped>
+</style>
